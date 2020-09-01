cc68beb9a11f98a95da95ec8e678f7171942c3b4
qiurenbo <qiurenbo@outlook.com>
2020-09-01 10:24:13 +0800

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NgZorroAntdModule } from './ng-zorro-antd.module';
+import { NgZorroAntdModule } from './nz.module';
+import { IconModule } from './nz-icon.module';
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, NgZorroAntdModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgZorroAntdModule,
+    IconModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/audio/audio.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio',
+  template: ``,
+  styleUrls: ['./audio.component.scss'],
+})
+export class AudioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
